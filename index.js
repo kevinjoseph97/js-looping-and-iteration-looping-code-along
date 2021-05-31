467b70a360e3a04f3bf2f4e010268d29fcfbe487
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-05-31 16:56:17 -0400

    commit tree 828947dd1cec4574fbfec0defa651fcee8c393ab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards (namesArray, event) {
+    let cards = [] 
+    for (let i = 0; i < namesArray.length; i++ ) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+
+function countDown (startingNiumber) {
+    while (startingNiumber > 0 ) {
+        console.log(startingNiumber); 
+        startingNiumber -= 1; 
+
+    }
+    console.log(startingNiumber);
+}
